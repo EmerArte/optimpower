81e2f4d12363e94f0d449747a0c3e0e4b50c29b5
Alejo <anieves@softcaribbean.com>
2023-01-19 11:46:59 -0500

    se consume servicio login y se crea el guard

--- optimpowerfront/src/app/app.module.ts
+++ optimpowerfront/src/app/app.module.ts
@@ -6,18 +6,20 @@ import { LoginComponent } from './components/login/login.component';
 import { FormularioLoginComponent } from './components/login/formulario-login/formulario-login.component';
 import { DashboardModule } from './components/dashboard/dashboard.module';
 import { HttpClientModule } from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     FormularioLoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    DashboardModule,
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        DashboardModule,
+        ReactiveFormsModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ optimpowerfront/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {};
+
+export const exposedApi = {
+
+  LOGIN_API: 'https://optimback.azurewebsites.net/api/auth/login',
+
+};
+
